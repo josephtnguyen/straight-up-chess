4b70e537ebac4aad608e89ab7799a9a56fe38555
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-20 12:52:17 -0700

    updated server to get all posted games

--- server/index.js
+++ server/index.js
@@ -2,12 +2,39 @@ require('dotenv/config');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+const pg = require('pg');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
+
+app.use(express.json());
 
 app.use(staticMiddleware);
 
 app.use(errorMiddleware);
+
+app.get('/api/get-posts', (req, res, next) => {
+  const sql = `
+  select *
+    from "postedGames"
+  `;
+
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => next(err));
+});
+
+app.post('/api/create-post', (req, res, next) => {
+
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
