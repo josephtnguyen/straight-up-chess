6050e63dd354abf9e6251467761a5a7d08553e73
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-20 15:47:01 -0700

    created Post Form

---
+++ client/pages/post-form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      side: 'white',
+      message: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className="post-form container page-height">
+
+      </div>
+    );
+  }
+}
