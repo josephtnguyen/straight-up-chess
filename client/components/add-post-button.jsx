d28b9f62d35dbfc5ea917da7b9ecf5970b1bee1e
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-20 15:38:43 -0700

    created add post button component

---
+++ client/components/add-post-button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AddPostButton(props) {
+  return (
+    <button className="add-post-btn justify-center align-items-center">
+      <img src="/images/add-icon.svg" />
+    </button>
+  );
+}
--- client/pages/join-game.jsx
+++ client/pages/join-game.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import AddPostButton from '../components/add-post-button';
 
 export default class JoinGame extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className="join-page container page-height">
+        <div className="row">
+          <div className="col d-flex justify-center">
+            <AddPostButton />
+          </div>
+        </div>
 
+        <div className="row">
+          <div className="col">
+
+          </div>
+        </div>
       </div>
     );
   }
