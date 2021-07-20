9cf60ce63616d980448faa44be6f361cd9fef0cd
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-20 11:47:15 -0700

    added schema with a table for holding posted games

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,40 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public.game" (
+	"gameId" serial NOT NULL,
+	"createdAt" timestamp with time zone NOT NULL,
+	CONSTRAINT "game_pk" PRIMARY KEY ("gameId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.moves" (
+	"gameId" integer NOT NULL,
+	"start" integer NOT NULL,
+	"end" integer NOT NULL,
+	"moveId" serial NOT NULL,
+	CONSTRAINT "moves_pk" PRIMARY KEY ("moveId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.postedGames" (
+	"gameId" integer NOT NULL,
+	"message" TEXT NOT NULL,
+	"createdAt" timestamp with time zone NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "moves" ADD CONSTRAINT "moves_fk0" FOREIGN KEY ("gameId") REFERENCES "game"("gameId");
+
+ALTER TABLE "postedGames" ADD CONSTRAINT "postedGames_fk0" FOREIGN KEY ("gameId") REFERENCES "game"("gameId");
