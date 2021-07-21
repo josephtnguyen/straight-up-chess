65bd473ec7b95e2079580b78bedf156ba6cde289
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-21 11:56:43 -0700

    Create a board component that renders the board based on Game.state.board

--- client/pages/game.jsx
+++ client/pages/game.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import ReactBoard from '../components/board';
+import Board from '../lib/board';
+import GameState from '../lib/gamestate';
 
 export default class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: null,
-      gamestate: null,
+      board: new Board(),
+      gamestate: new GameState(),
       player: 'w'
     };
   }
 
   render() {
+    const { board } = this.state;
     return (
       <div className="game container page-height">
-
+        <ReactBoard board={board} />
       </div>
     );
   }
