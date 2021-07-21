1ecabac4ab327a01ea5e1c9061e0783681236fd1
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-21 16:45:37 -0700

    made Game page mobile responsive

--- client/pages/game.jsx
+++ client/pages/game.jsx
@@ -31,12 +31,32 @@ export default class Game extends React.Component {
     const { board } = this.state;
     const player = { username: 'Anonymous' };
     return (
-      <div className="game container page-height">
-        <PlayerPalette player={null} cancelAction={this.cancelGame} />
-        <div className="board-container">
-          <ReactBoard board={board} />
+      <div className="game page-height mx-auto">
+        <div className="w-100 d-block d-md-none p-2">
+          <PlayerPalette player={null} cancelAction={this.cancelGame} />
         </div>
-        <PlayerPalette player={player} />
+
+        <div className="w-100 row">
+          <div className="col">
+
+            <div className="board-container my-2">
+              <ReactBoard board={board} />
+            </div>
+          </div>
+
+          <div className="col-auto d-none d-md-block">
+            <div className="w-100 p-2">
+              <PlayerPalette player={null} cancelAction={this.cancelGame} />
+            </div>
+            <div className="w-100 p-2">
+              <PlayerPalette player={player} />
+            </div>
+          </div>
+        </div>
+
+        <div className="w-100 d-block d-md-none p-2">
+          <PlayerPalette player={player} />
+        </div>
       </div>
     );
   }
