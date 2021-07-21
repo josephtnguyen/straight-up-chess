70f6db975bb7143fdd9ccfa15db96f824d019ddf
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-21 10:25:36 -0700

    refactored coords module to es6

--- client/lib/coords.js
+++ client/lib/coords.js
@@ -1,17 +1,19 @@
-
-export default function Coords() {
-  Object.setPrototypeOf(this, Array.prototype);
-  for (let i = 10; i < 90; i += 10) {
-    for (let j = 1; j < 9; j++) {
-      this.push(i + j);
+export default class Coords {
+  constructor() {
+    Object.setPrototypeOf(this, Array.prototype);
+    for (let i = 10; i < 90; i += 10) {
+      for (let j = 1; j < 9; j++) {
+        this.push(i + j);
+      }
     }
   }
-  this.isCoord = function (number) {
+
+  isCoord(number) {
     for (const coord of this) {
       if (number === coord) {
         return true;
       }
     }
     return false;
-  };
+  }
 }
