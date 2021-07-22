06153ad5e72610d0577b1b5b557c4fce1b3dcb30
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-21 18:12:19 -0700

    fixed coords module

--- client/lib/coords.js
+++ client/lib/coords.js
@@ -1,6 +1,6 @@
-export default class Coords {
-  constructor() {
-    Object.setPrototypeOf(this, Array.prototype);
+export default class Coords extends Array {
+  constructor(...args) {
+    super(...args);
     for (let i = 10; i < 90; i += 10) {
       for (let j = 1; j < 9; j++) {
         this.push(i + j);
