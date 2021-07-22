b9f3054436b04f423182ce243832ed2b2fd0f309
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-22 12:50:10 -0700

    flipped displayed side of post to reflect side of the user

--- client/components/post.jsx
+++ client/components/post.jsx
@@ -4,13 +4,16 @@ import Board from '../lib/board';
 
 export default function Post(props) {
   const { meta } = props;
+  const side = meta.playerSide === 'white'
+    ? 'black'
+    : 'white';
   const board = new Board();
   return (
     <div className="post p-2">
       <div className="row">
         <div className="col-4">
           <div className="post-board-container">
-            <ReactBoard board={board} side={meta.playerSide} />
+            <ReactBoard board={board} side={side} />
           </div>
         </div>
         <div className="col post-text">
