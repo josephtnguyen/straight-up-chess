d39003961de12e03876b4c7520dc4884fb13d010
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-28 18:00:12 -0700

    Add a new 'users' table with 'userId', 'username', 'hashedPassword', 'createdAt' columns

--- database/data.sql
+++ database/data.sql
@@ -2,3 +2,7 @@ insert into "games" ("message", "playerName", "playerSide", "opponentSide")
 values ('play me', 'Anonymous', 'black', 'white'),
        ('please go easy on me ;)', 'username12', 'white', 'black')
 returning *;
+
+insert into "users" ("username", "hashedPassword")
+values ('testing', 'lololol')
+returning *;
