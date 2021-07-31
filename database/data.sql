688e2dcb4cd3b39e1f859671ccee2b71dfaeaa82
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-31 11:13:12 -0700

    removed placeholder games

--- database/data.sql
+++ database/data.sql
@@ -1,8 +1,3 @@
-insert into "games" ("message", "playerName", "playerSide", "opponentSide")
-values ('play me', 'Anonymous', 'black', 'white'),
-       ('please go easy on me ;)', 'username12', 'white', 'black')
-returning *;
-
 insert into "users" ("username", "hashedPassword")
 values ('Anonymous', '$argon2i$v=19$m=4096,t=3,p=1$fXkHPjxT6IK9v4B24KSg5g$7l5Z1ToiLwwznekaFDcfGFqhESXHskA5nuDvxPXLJ78')
 returning *;
