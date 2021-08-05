89d59853f98058acb76e25abb6f7902a23c0611e
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-08-05 11:14:30 -0700

    added a demo account

--- database/data.sql
+++ database/data.sql
@@ -1,3 +1,4 @@
 insert into "users" ("username", "hashedPassword")
-values ('Anonymous', '$argon2i$v=19$m=4096,t=3,p=1$fXkHPjxT6IK9v4B24KSg5g$7l5Z1ToiLwwznekaFDcfGFqhESXHskA5nuDvxPXLJ78')
+values ('Anonymous', '$argon2i$v=19$m=4096,t=3,p=1$fXkHPjxT6IK9v4B24KSg5g$7l5Z1ToiLwwznekaFDcfGFqhESXHskA5nuDvxPXLJ78'),
+       ('demo', '$argon2i$v=19$m=4096,t=3,p=1$l9mGKtyofrl4GxF5xuI2KQ$eTxyN2L+0l4OFDxnyY1uoUl+NFPC7mB/jjwVqr7QTJQ')
 returning *;
